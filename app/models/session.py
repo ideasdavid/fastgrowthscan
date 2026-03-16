11d09f5ce1f14b4e5e4383143211a7f800e01953
davidwilliams1601 <davidwilliams1601@gmail.com>
2026-03-16 10:35:53 +0000

    Fix session.py syntax error

--- app/models/session.py
+++ app/models/session.py
@@ -1,10 +1,11 @@
-_db_url = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-engine = create_engine(_db_url, echo=False)from sqlalchemy import create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from app.config import DATABASE_URL
 from app.models.db import Base
 
-engine = create_engine(DATABASE_URL, echo=False)
+_db_url = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(_db_url, echo=False)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 
